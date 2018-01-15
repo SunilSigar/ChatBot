e077234a316bca9a04d4e86c081830c80e70b7dd
Sigar <sunil.sigar@accenture.com>
2018-01-15 14:57:12 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+// import { HttpClient} from '@angular/common/http';
+import { Http, Response } from '@angular/http';
+import { Headers, RequestOptions } from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  inputMsg = ''; // text entered by user
+  conversation = []; // hold all msg of user and bot
+
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+    //this.login();
+  }
+
+  dataObj = {}; // hold username and pwd
+  
+  //login user for first time
+  login(){  
+                console.log("inside login");
+                this.dataObj['userName']='a1@gmail.com';
+                this.dataObj['password']='123';
+
+                let headers = new Headers({ 'Content-Type': 'application/json',
+                                'Authorization': 'Bearer 25d94a24-eec6-38aa-bf39-aedbe0964107' });
+                let options = new RequestOptions({ headers: headers });
+                let url: "https://csva-pension-bot-controller-dot-acn-csva-project.appspot.com/login/authenticate";                         
+                let data = this.dataObj;                                
+
+                let res = this.http.post(url, data, options).map((res: Response) => res.json());
+                console.log(JSON.stringify(res));
+                
+  }
+
+  keypressed($event){
+    if($event.keyCode == 13) {
+      this.addMsg();
+      this.login();
+    }
+  }
+
+  addMsg(){
+    this.conversation.push(this.inputMsg);
+    this.callHttp();
+    this.inputMsg = '';
+    console.log(this.conversation);
+  }
+
+  contextsvalue = {};
+  callHttp(){
+    // this.http.post()
+  }
+}
